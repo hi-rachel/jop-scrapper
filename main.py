d09267bb76b4167b767ca4bb2d36dee44ef83f86
Rachel <rachel.uiux@gmail.com>
2022-09-14 00:45:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+app = Flask("JobScrapper")
+
+
+@app.route("/")
+def home():
+    return 'hi there!'
+
+
+app.run("0.0.0.0")
+
+
+"""
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
+from extractors.wanted import extract_wanted_jobs
+from file import save_to_file
+
+keyword = input("What do you want to search for?")
+
+indeed = extract_indeed_jobs(keyword)
+
+wwr = extract_wwr_jobs(keyword)
+
+wanted = extract_wanted_jobs(keyword)
+
+jobs = indeed + wwr + wanted
+
+save_to_file(keyword, jobs)
+"""
