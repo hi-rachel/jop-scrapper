c2ec05cbac0b15f3a7717754e65c72cb68ac87dc
Rachel <rachel.uiux@gmail.com>
2022-09-17 09:36:41 +0000

    Build a website with Flask - get the job data

--- extractors/wanted.py
+++ extractors/wanted.py
@@ -1,37 +1,43 @@
-from requests import get
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def extract_wanted_jobs(keyword):
-  options = Options()
-  options.add_argument("--no-sandbox")
-  options.add_argument("--disable-dev-shm-usage")
-  browser = webdriver.Chrome(options=options)
-  
-  base_url = "https://www.wanted.co.kr/search?query="
-  
-  browser.get(f"{base_url}{keyword}")
-  
-  print("Requesting", f"{base_url}{keyword}")
-  soup = BeautifulSoup(browser.page_source, 'html.parser')
-  results = []
-  jobs = soup.find_all('ul', class_="clearfix")
-  for list in jobs:
-    job_list = list.find_all('li', recursive=False)
-    print(len(job_list))
-    for anchors in job_list:
-      anchors = list.find_all('a')
-      for a in anchors:
-        link = a['href']
-        title = a.find('div', class_="job-card-position").get_text(),
-        company = a.find('div', class_="job-card-company-name").get_text(),
-        location = a.find('div', class_="job-card-company-location").get_text()
-        job_data = {
-          'link' : f"https://www.wanted.co.kr{link}",
-          'company' : " ".join(str(x) for x in company).replace(",", " "),
-          'location' : " ".join(str(x) for x in location).replace(",", " "),
-          'position' : " ".join(str(x) for x in title).replace(",", " "),
-        }
-        results.append(job_data)
-  return results
+    options = Options()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    browser = webdriver.Chrome(options=options)
+
+    base_url = "https://www.wanted.co.kr/search?query="
+
+    browser.get(f"{base_url}{keyword}")
+
+    print("Requesting", f"{base_url}{keyword}")
+    soup = BeautifulSoup(browser.page_source, 'html.parser')
+    results = []
+    jobs = soup.find_all('ul', class_="clearfix")
+    for list in jobs:
+        job_list = list.find_all('li', recursive=False)
+        print(len(job_list))
+        for anchors in job_list:
+            anchors = list.find_all('a')
+            for a in anchors:
+                link = a['href']
+                title = a.find('div', class_="job-card-position").get_text(),
+                company = a.find('div',
+                                 class_="job-card-company-name").get_text(),
+                location = a.find(
+                    'div', class_="job-card-company-location").get_text()
+                job_data = {
+                    'link':
+                    f"https://www.wanted.co.kr{link}",
+                    'company':
+                    " ".join(str(x) for x in company).replace(",", " "),
+                    'location':
+                    " ".join(str(x) for x in location).replace(",", " "),
+                    'position':
+                    " ".join(str(x) for x in title).replace(",", " "),
+                }
+                results.append(job_data)
+    return results
--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+from flask import Flask, render_template, request
+from extractors.indeed import extract_indeed_jobs
+from extractors.wwr import extract_wwr_jobs
+from extractors.wanted import extract_wanted_jobs
+
+app = Flask("JobScrapper")
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/search")
+def search():
+    keyword = request.args.get("keyword")
+    indeed = extract_indeed_jobs(keyword)
+    wwr = extract_wwr_jobs(keyword)
+    wanted = extract_wanted_jobs(keyword)
+    jobs = indeed + wwr + wanted
+    return render_template("search.html", keyword=keyword, jobs=jobs)
+
+
+app.run("0.0.0.0")
+
+"""
 from extractors.indeed import extract_indeed_jobs
 from extractors.wwr import extract_wwr_jobs
 from extractors.wanted import extract_wanted_jobs
@@ -14,3 +40,4 @@ wanted = extract_wanted_jobs(keyword)
 jobs = indeed + wwr + wanted
 
 save_to_file(keyword, jobs)
+"""
